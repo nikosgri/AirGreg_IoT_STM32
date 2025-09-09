dda3c64b5e53d8c0d6e70ba5d864018b6657e5d5
=nikosgri <=n.grigoriadis09@gmail.com>
2025-09-09 18:57:18 +0300

    Start implementation for CO2 sensor drivers

---
+++ Peripherals/Inc/scd41.h
@@ -0,0 +1,22 @@
+/*
+ * scd41.h
+ *
+ *  Created on: Sep 6, 2025
+ *      Author: Nikolaos Grigoriadis
+ *      Email : n.grigoriadis09@gmail.com
+ *      Title : Embedded software engineer
+ *      Degree: BSc and MSc in computer science, university of Ioannina
+ */
+
+#ifndef INC_SCD41_H_
+#define INC_SCD41_H_
+
+#include <main.h>
+
+#define SDC41_ADDRESS 0x62
+
+
+
+
+
+#endif /* INC_SCD41_H_ */
---
+++ Peripherals/Src/scd41.c
@@ -0,0 +1,12 @@
+/*
+ * scd41.c
+ *
+ *  Created on: Sep 6, 2025
+ *      Author: Nikolaos Grigoriadis
+ *      Email : n.grigoriadis09@gmail.com
+ *      Title : Embedded software engineer
+ *      Degree: BSc and MSc in computer science, university of Ioannina
+ *      Description: CO2 measurement
+ */
+
+#include "scd41.h"
