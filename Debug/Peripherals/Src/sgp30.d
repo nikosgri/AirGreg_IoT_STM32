993707daa4790ebba1471baecdd161434843af25
=nikosgri <=n.grigoriadis09@gmail.com>
2026-04-30 10:27:25 +0300

    Remove build artifacts and add .gitignore

--- Debug/Peripherals/Src/sgp30.d
+++
@@ -1,3 +0,0 @@
-Peripherals/Src/sgp30.o: ../Peripherals/Src/sgp30.c \
- /home/grego/STM32CubeIDE/workspace/AirGreg_IoT_STM32/Peripherals/Inc/sgp30.h
-/home/grego/STM32CubeIDE/workspace/AirGreg_IoT_STM32/Peripherals/Inc/sgp30.h:
--- Debug/Startup/startup_stm32l053r8tx.d
+++
@@ -1,1 +0,0 @@
-Startup/startup_stm32l053r8tx.o: ../Startup/startup_stm32l053r8tx.s
